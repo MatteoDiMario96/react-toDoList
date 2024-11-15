e3c47ab2e016acb1e941558ec90caa5a7a2993be
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-15 01:44:59 +0100

    Put a todos, made persisting data, handlNewTodo, handleEditTodo, handleDeleteTodo, put the two components with the props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import ToDoInput from "./assets/components/ToDoInput";
+import ToDoList from "./assets/components/ToDoList";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[todos, setTodos] = useState([
+    'Learn React',
+    'Make good project',
+    'Publish on GitHub',
+    'Publish on Linkedin'
+  ])
+
+  const[toDoValue, setToDoValue] = useState('');
+
+  function persistData(newList){
+    localStorage.setItem('todos', JSON.stringify({ todos: newList}))
+  }
+
+  function handleNewTodo(newToDo){
+    const newToDoList = [...todos, newToDo];
+    persistData(newToDoList)
+    setTodos(newToDoList);
+  }
+
+  function handleDeleteToDo(index){
+      const newToDoList = todos.filter((todo, todoIndex) =>{
+        return todoIndex !== index
+      })
+      persistData(newToDoList)
+      setTodos(newToDoList)
+  }
+
+  
+
+  function handleEditToDo(index){
+    const valueToBeEdited = todos[index]
+    setToDoValue(valueToBeEdited)
+    handleDeleteToDo(index)
+  }
+
+  useEffect(() => {
+    if(!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if(!localTodos){
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+          <header className="header">
+              <h1>To-Do-List in React</h1>
+          </header>
+          <ToDoInput
+              handleNewTodo={handleNewTodo}
+              toDoValue={toDoValue}
+              setToDoValue={setToDoValue}
+          />
+          <ToDoList 
+              todos={todos}
+              handleDeleteToDo={handleDeleteToDo}
+              handleEditToDo={handleEditToDo}
+          />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
